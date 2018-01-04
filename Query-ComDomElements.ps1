1023a8ab54142d5bc4c01620de5a169078dfd307
Guenther Schmitz <gpunktscripts@gpunktschmitz.com>
2018-01-04 14:32:23 +0100

    renamed parameter 'Filter' to 'Query'

--- Query-ComDomElements.ps1
+++ Query-ComDomElements.ps1
@@ -1,49 +1,49 @@
-function Query-ComDomElements([string]$Filter, $Dom = $false, $Property = $false) {
+function Query-ComDomElements([string]$Query, $Dom = $false, $Property = $false) {
 	if($Dom -eq $false -or $Dom -eq $null) {
 		throw "DOM not valid or set"
 	}
 	
-	$SecondFilter = $false
-	if($Filter.IndexOf('>') -ne -1) {
-		$SecondFilter = $Filter.Substring($Filter.IndexOf('>')+1).Trim()
-		$Filter = $Filter.Substring(0,$Filter.IndexOf('>')).Trim()
+	$SecondQuery = $false
+	if($Query.IndexOf('>') -ne -1) {
+		$SecondQuery = $Query.Substring($Query.IndexOf('>')+1).Trim()
+		$Query = $Query.Substring(0,$Query.IndexOf('>')).Trim()
 	}
 	
-	switch($Filter.Substring(0,1)) {
+	switch($Query.Substring(0,1)) {
 		'.' {
-			if($Property -ne $false -and $SecondFilter -eq $false) {
-				$result = [System.__ComObject].InvokeMember("getElementsByClassName",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Filter.Substring(1)) | select $Property
+			if($Property -ne $false -and $SecondQuery -eq $false) {
+				$result = [System.__ComObject].InvokeMember("getElementsByClassName",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Query.Substring(1)) | select $Property
 			} else {
-				$result = [System.__ComObject].InvokeMember("getElementsByClassName",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Filter.Substring(1))
+				$result = [System.__ComObject].InvokeMember("getElementsByClassName",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Query.Substring(1))
 			}
 			Break
 		}
 		'#' {
-			if($Property -ne $false -and $SecondFilter -eq $false) {
-				$result = [System.__ComObject].InvokeMember("getElementById",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Filter.Substring(1)) | select $Property
+			if($Property -ne $false -and $SecondQuery -eq $false) {
+				$result = [System.__ComObject].InvokeMember("getElementById",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Query.Substring(1)) | select $Property
 			} else {
-				$result = [System.__ComObject].InvokeMember("getElementById",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Filter.Substring(1))
+				$result = [System.__ComObject].InvokeMember("getElementById",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Query.Substring(1))
 			}
 			Break
 		}
 		default {
-			if($Property -ne $false -and $SecondFilter -eq $false) {
-				$result = [System.__ComObject].InvokeMember("getElementsByTagName",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Filter) | select $Property
+			if($Property -ne $false -and $SecondQuery -eq $false) {
+				$result = [System.__ComObject].InvokeMember("getElementsByTagName",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Query) | select $Property
 			} else {
-				$result = [System.__ComObject].InvokeMember("getElementsByTagName",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Filter)
+				$result = [System.__ComObject].InvokeMember("getElementsByTagName",[System.Reflection.BindingFlags]::InvokeMethod, $null, $dom, $Query)
 			}
 			Break
 		}
 	}
 	
-	if($SecondFilter -eq $false) {
+	if($SecondQuery -eq $false) {
 		if($Property -ne $false) {
 			return $result.$Property
 		} else {
 			return $result
 		}
 	} else {
-		return Query-ComDomElements -Filter $SecondFilter -Dom $result -Property $Property
+		return Query-ComDomElements -Query $SecondQuery -Dom $result -Property $Property
 	}
 }
 
